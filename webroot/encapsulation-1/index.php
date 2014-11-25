fa974f5890856e283a42c4cf22d14937aaee86a4
Red de Leon <red@athleticsnyc.com>
2014-11-25 12:50:41 -0500

    Add encapsulation example

---
+++ webroot/encapsulation-1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Less Encapsulation</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <style type="text/css">
+  </style>
+</head>
+<body>
+  <div class="page">
+    <header>
+      <h1>Less Encapsulation</h1>
+    </header> 
+    <div class="box box-1">
+      <p>box-1</p>
+    </div>
+    <div class="box box-2">
+      <p>box-2</p>
+    </div>
+    <div class="box box-3">
+      <p>box-3</p>
+    </div>
+    <div class="box box-4">
+      <p>box-4</p>
+    </div>
+  </div>
+</body>
+</html>
